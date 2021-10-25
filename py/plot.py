fixed scaling problem with human arm data

py/plot.py
@@ -5,6 +5,52 @@ import pandas as pd
 
 colors=['green', 'blue', 'orange', 'red', 'purple']
 
+def get_cov_ellipsoid(cov, mu=np.zeros((3)), nstd=3):
+    """
+    Return the 3d points representing the covariance matrix
+    cov centred at mu and scaled by the factor nstd.
+    Plot on your favourite 3d axis. 
+    Example 1:  ax.plot_wireframe(X,Y,Z,alpha=0.1)
+    Example 2:  ax.plot_surface(X,Y,Z,alpha=0.1)
+    """
+    assert cov.shape==(3,3)
+
+    # Find and sort eigenvalues to correspond to the covariance matrix
+    eigvals, eigvecs = np.linalg.eigh(cov)
+    idx = np.sum(cov,axis=0).argsort()
+    eigvals_temp = eigvals[idx]
+    idx = eigvals_temp.argsort()
+    eigvals = eigvals[idx]
+    eigvecs = eigvecs[:,idx]
+
+    # Set of all spherical angles to draw our ellipsoid
+    n_points = 100
+    theta = np.linspace(0, 2*np.pi, n_points)
+    phi = np.linspace(0, np.pi, n_points)
+
+    # Width, height and depth of ellipsoid
+    rx, ry, rz = nstd * np.sqrt(eigvals)
+
+    # Get the xyz points for plotting
+    # Cartesian coordinates that correspond to the spherical angles:
+    X = rx * np.outer(np.cos(theta), np.sin(phi))
+    Y = ry * np.outer(np.sin(theta), np.sin(phi))
+    Z = rz * np.outer(np.ones_like(theta), np.cos(phi))
+
+    # Rotate ellipsoid for off axis alignment
+    old_shape = X.shape
+    # Flatten to vectorise rotation
+    X,Y,Z = X.flatten(), Y.flatten(), Z.flatten()
+    X,Y,Z = np.matmul(eigvecs, np.array([X,Y,Z]))
+    X,Y,Z = X.reshape(old_shape), Y.reshape(old_shape), Z.reshape(old_shape)
+   
+    # Add in offsets for the mean
+    X = X + mu[0]
+    Y = Y + mu[1]
+    Z = Z + mu[2]
+    
+    return X,Y,Z
+
 fig = plt.figure()
 ax = plt.axes(projection='3d')
 plt.title('Demonstrations')
@@ -21,13 +67,34 @@ for i in np.arange(5):
 	ax.scatter3D(xdata, ydata, zdata, c=colors[i])
 '''
 
-data_path = "/home/nnrthmr/C_Mat.csv"
-data = pd.read_csv(data_path, sep=",")
-data=data.append(data.loc[1]*0.75)
 
-xdata= np.array(data.iloc[0])
-ydata= np.array(data.iloc[1])
-zdata= np.array(data.iloc[2])
+###############################################
+# human arm motion data                       #
+###############################################
+'''
+    First 12 values are the joint values
+    values 13-15 are the wrist position
+    values 16-18 are the wrist orientation in xyz
+    values 19-54 are the wrist position jacobian (first row, then second row, then third row)
+    values 55-90 are the wrist orientation jacobian (in the same format as above)
+'''
+data_path = "/home/nnrthmr/CLionProjects/ma_thesis/data/demos/human_arm/humanArmMotionOutput.txt"
+data = pd.read_csv(data_path, sep=" ", skiprows=1, header=None)
+
+xdata= np.array(data[12])
+ydata= np.array(data[13])
+zdata= np.array(data[14])
+print(data[12:15])
+manip=list()
+
+for i in np.arange(0, 8219):
+    tmp=np.array(data.iloc[i][18:54]).reshape((3,12))
+    manip.append(0.000001*np.matmul(tmp,np.transpose(tmp)))
+
+for i in np.arange(0,len(manip),150):
+    m_i = manip[i]
+    X2,Y2,Z2 = get_cov_ellipsoid(m_i, [xdata[i],ydata[i],zdata[i]], 1)
+    ax.plot_wireframe(X2,Y2,Z2, color='grey', alpha=0.05)
 
 ax.scatter3D(xdata, ydata, zdata, c=colors[0])
 

py/plotAll3dHumanArm.py
@@ -60,7 +60,7 @@ def get_cov_ellipsoid(cov, mu=np.zeros((3)), nstd=3):
 ###########################
 ### plot demonstrations ###
 
-n_demos=2
+n_demos=1
 #n_points=20
 n_points=400
 scaling_factor=1e-6
@@ -76,14 +76,16 @@ plt.title('Demonstrations and GMR results')
 #data_path = "/home/nnrthmr/Desktop/master-thesis/promps-code/tum_tuda_project/recorded_data/Session_25_06_2021/run1.csv"
 #data_path = "../data/demos/trajectories.csv"
 data_path = "../data/demos/human_arm/dummyTrajectories.csv"
-data = pd.read_csv(data_path, sep=",")
+data = pd.read_csv(data_path, sep=",", names=['s','EE_x','EE_y','EE_z'])
+#data = pd.read_csv(data_path, sep=",")
+print(data)
 xdata= np.array(data['EE_x'])[:n_demos*n_points]
 ydata= np.array(data['EE_y'])[:n_demos*n_points]
 zdata= np.array(data['EE_z'])[:n_demos*n_points]
 #xdata= np.array(data['panda_left_EE_x'])
 #ydata= np.array(data['panda_left_EE_y'])
 #zdata= np.array(data['panda_left_EE_z'])
-ax.scatter3D(xdata, ydata, zdata, c='grey', alpha=0.4)
+ax.scatter3D(xdata, ydata, zdata, c='grey', alpha=0.2)
 ax.scatter3D(0,0,0, c='red', marker='x', s=100)
 
 
@@ -97,7 +99,6 @@ manip_tmp=manip_tmp[1:,:]
 manip=list()
 
 for i in np.arange(0, manip_tmp.shape[0]):
-    #manip.append(scaling_factor*manip_tmp[i,1:].reshape(3,3))
     manip.append(scaling_factor*manip_tmp[i,:].reshape(3,3))
 
 manip=manip[:n_demos*n_points]
@@ -113,23 +114,24 @@ for i in np.arange(0,len(manip),plot_every_nth):
 #filename_mu = "../data/expData3d.csv"
 #filename_sigma= "../data/expDataSPD3d.csv"
 filename_mu = "../data/results/human_arm/xd.csv"
+filename_sigma= "../data/results/human_arm/xhat.csv"
 
 mu_tmp = genfromtxt(filename_mu, delimiter=',')
-#sigma_tmp = genfromtxt(filename_sigma, delimiter=',')
-#sigma_tmp = scaling_factor*sigma_tmp
+sigma_tmp = genfromtxt(filename_sigma, delimiter=',')
+sigma_tmp = scaling_factor*sigma_tmp
 
 mu=list()
-#sigma=list()
+sigma=list()
 
 for i in np.arange(n_points):
     mu.append(mu_tmp[:,i])
-    #sigma.append(sigma_tmp[i,:].reshape(3,3))
+    sigma.append(sigma_tmp[i,:].reshape(3,3))
 
 for i in np.arange(n_points):
     mu_i = mu[i]
-    ax.scatter(mu_i[0],mu_i[1],mu_i[2], color='blue')
+    ax.scatter(mu_i[0],mu_i[1],mu_i[2], color='blue', alpha=0.2)
 
-'''
+
 for i in np.arange(0,n_points,plot_every_nth):
     mu_i = mu[i]
     sigma_i=sigma[i]
@@ -142,7 +144,7 @@ for i in np.arange(0,n_points,plot_every_nth):
 ###############################
 ### plot cone in SPD space  ###
 
-'''
+
 
 import matplotlib.patches as mpatches
 
@@ -151,12 +153,13 @@ blue_patch = mpatches.Patch(color='blue', label='Learned')
 grey_patch = mpatches.Patch(color='grey', label='Demonstrated')
 
 plt.legend(handles=[red_patch, blue_patch, grey_patch])
-#plt.xlim(-0.5, 0.5)
-#plt.ylim(-1, 1)
 ax.set_zlim(-0.275, -0.305)
 plt.xlim(0.31, 0.318)
 plt.ylim(0.2225, 0.24)
-#ax.set_zlim(0., 1)
+
+#ax.set_zlim(-0.27, -0.28)
+#plt.xlim(0.316, 0.318)
+#plt.ylim(0.234, 0.24)
 plt.show()
 
 